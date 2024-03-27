1c051f0958e94f21dd3b4b9a3a92320bc9ae7602
Desenvolvimento <dsadm@educ501.local>
2024-03-26 22:55:10 -0300

    aula 26-03-24

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField("email address", unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    registrationNumber = models.CharField(max_length=30)
+    phoneNumber = models.CharField(max_length=15, null=True, blank=True)
+    is_email_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    #objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
 
 BLOCKS = [
     ("A","Bloco A"),
@@ -36,7 +56,7 @@ TASKS_STATUS = [
 
 class Tasks(models.Model):
     environmentFK = models.ForeignKey(Environments, related_name='tasksEnvironments', on_delete=models.CASCADE)
-    #reporterFK = models.ForeignKey(CustomUser, related_name='tasksCustomUser', on_delete=models.CASCADE)
+    reporterFK = models.ForeignKey(CustomUser, related_name='tasksCustomUser', on_delete=models.CASCADE)
     creationDate = models.DateTimeField(auto_now_add=True)    
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=2000)
@@ -47,3 +67,47 @@ class Tasks(models.Model):
     def __str__(self):
         return self.title
 
+class TasksAssignees(models.Model):
+    assigneeFK = models.ForeignKey(CustomUser, related_name='tasksAssigneesCustomUser', on_delete=models.CASCADE)
+    taskFK = models.ForeignKey(Tasks, related_name='tasksAssigneesTask', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.taskFK.title
+    
+
+class TasksStatus(models.Model):    
+    taskFK = models.ForeignKey(Tasks, related_name='tasksStatusTask', on_delete=models.CASCADE)
+    status = models.CharField(max_length=30,choices=TASKS_STATUS)
+    creationDate = models.DateTimeField(auto_now_add=True)    
+    comment = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.status
+
+class TasksEquipments(models.Model):    
+    taskFK = models.ForeignKey(Tasks, related_name='tasksEquipmentsTask', on_delete=models.CASCADE)
+    equipmentFK = models.ForeignKey(Equipments, related_name='tasksEquipmentsEquipment', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.taskFK.title
+
+FILE_TYPE = [
+    ('D','Document'),
+    ('P','Photo')
+]
+    
+class FileTasksStatus(models.Model):    
+    taskStatusFK = models.ForeignKey(TasksStatus, related_name='fileTasksStatusTask', on_delete=models.CASCADE)
+    link = models.CharField(max_length=1000)
+    fileType = models.CharField(max_length=15,choices=FILE_TYPE)    
+        
+    def __str__(self):
+        return self.fileType
+
+
+class EnvironmentsAssignees(models.Model):    
+    environmentFK = models.ForeignKey(Environments, related_name='environmentsAssigneesEnv', on_delete=models.CASCADE)
+    assigneeFK = models.ForeignKey(CustomUser, related_name='environmentsAssigneesUser', on_delete=models.CASCADE)
+        
+    def __str__(self):
+        return self.assigneeFK.email
