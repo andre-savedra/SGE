35f85a961de6c6a5f7eae936a50c8998884d0b6e
Desenvolvimento <dsadm@educ501.local>
2024-04-09 19:16:57 -0300

    added static files server

--- backend/sge/settings.py
+++ backend/sge/settings.py
@@ -58,11 +58,13 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CSRF_TRUSTED_ORIGINS = ['*']
 
 ROOT_URLCONF = 'sge.urls'
 
